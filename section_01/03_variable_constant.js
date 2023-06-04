3e5fd30cd90d5243d07f5e3a8a87196cf8da4c42
slamdunk7575 <slamdunk7575@gmail.com>
2023-06-04 16:50:57 +0900

    (feat) 섹션_01 03.변수와 상수

---
+++ section_01/03_variable_constant.js
@@ -0,0 +1,10 @@
+console.log('Hello,', '철수');
+
+// 값들을 주머니에 담아 사용하기
+const SALUTATION = 'Hello,';
+let person = '철수';
+
+console.log(SALUTATION, person);
+
+person = '영희';
+console.log(SALUTATION, person);
